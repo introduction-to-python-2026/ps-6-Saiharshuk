554faa2538780da502fdaf9348c46bfb52cc4f4f
Saiharshuk <sai.harshuk@live.biu.ac.il>
2025-11-30 16:05:27 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,13 @@
-def create_codon_dict(file_path):
-    pass # Replace the pass with your code
 
+   def create_codon_dict(file_path):
+    file = open(file_path) 
+    rows = file.readlines()
+    file.close()
+    map = {}
+    for row in rows:
+      cells = row.strip().split('/t')
+      codon = cells(0)
+      amino_acid = cells (2)
+      map[codon]= amino_acid
+    return
 
